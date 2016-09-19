c1e535bff70a06ed2aa219a7ff9dfdfd26ce30ba
Ahljun <cloudtrial43@gmail.com>
2016-09-19 20:13:38 +1000

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,17 @@
     
     <form action="/sign" method="post">
         <div>Height(cm) <textarea name="height" rows="1" cols="3"></textarea></div><br>
-	    <div>Weight(kg) <textarea name="weight" rows="1" cols="3"></textarea></div><br>
+	<div>Weight(kg) <textarea name="weight" rows="1" cols="3"></textarea></div><br>
 	  
         <div><input type="submit" value="Submit" name="makeResults"></div>
     </form>
     </body>
 </html>
+
+<?php
+$height = ($POST['height']);
+$height = ($POST['weight']);
+$BMI = $height + $wright;
+
+echo $BMI;
+?>
