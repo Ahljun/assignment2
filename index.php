36fe63a413f276d3d483fda3c3ff53b3ebccea42
Ahljun <cloudtrial43@gmail.com>
2016-09-19 17:46:27 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <header>
+	    <title>iLift</title>
+	</header>
+    <body>
+    
+    <form action="/sign" method="post">
+        <div>Height(cm) <textarea name="height" rows="1" cols="3"></textarea></div><br>
+	    <div>Weight(kg) <textarea name="weight" rows="1" cols="3"></textarea></div><br>
+	  
+        <div><input type="submit" value="Submit" name="makeResults"></div>
+    </form>
+    </body>
+</html>
