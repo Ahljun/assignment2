f20d5f82079c97a692c0156985f0904eb66512f9
Ahljun <cloudtrial43@gmail.com>
2016-09-22 16:11:50 +1000

    no negative vals
    no negative vals and no

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 <form action="" method="post">
 
     <label>Weight(kg):</label>
-    <input type="number" name="weight" rows="1" cols="3"/><br><br>
+    <input type="number" name="weight" min="0"/><br><br>
 
-    <label>Height(cm):</label>
-    <input type="number" name="height" step="0.01" rows="1" cols="3"/><br><br>
+    <label>Height(m):</label>
+    <input type="number" name="height" step="0.01" min="0"/><br><br>
 
     <input  type="submit" name="btn_submit" value="Submit">
 </form>
@@ -23,7 +23,7 @@
         
         $BMI =  ($weight/($height*$height));
         
-        echo "The total value is: ".$BMI;
+        echo "Your BMI is: ".$BMI;
     }
 ?>
 
