2126235fe67b880b9aa8cefa5e21956867be40a6
andrewauxilio <andrew.auxilio@gmail.com>
2016-09-22 12:55:04 +1000

    storage
    store in cloud

--- index.php
+++ index.php
@@ -1,32 +1,44 @@
 <html>
-<head>
+  
+  <head>
     <title>iLift</title>
-</head>
-<body>
+  </head>
 
-<form action="" method="post">
-
-    <label>Weight(kg):</label>
-    <input type="number" name="weight" rows="1" cols="3"/><br><br>
-
-    <label>Height(cm):</label>
-    <input type="number" name="height" step="0.01" rows="1" cols="3"/><br><br>
-
-    <input  type="submit" name="btn_submit" value="Submit">
-</form>
-
-<?php
-    if(isset($_POST['btn_submit']))
-    {
-        $weight = $_POST['weight'];
-        $height = $_POST['height'];
+  <?php
+  
+      $handle = fopen ('gs://s3488990-storage/bmi.txt','w');
+      fwrite ($handle, "2");
+  
+      if(isset($_POST['btn_submit']))
+      
+      {
+          $weight = $_POST['weight'];
+          $height = $_POST['height'];
+          
+          $BMI =  ($weight/($height*$height));
+      }
+      
+      fwrite ($handle, ", " . $BMI);
+      break;  
+      
+  ?>
+  
+  <body>
+  
+    <form action="" method="post">
+    
+        <label>Weight(kg):</label>
+        <input type="number" name="weight" rows="1" cols="3"/><br><br>
+    
+        <label>Height(cm):</label>
+        <input type="number" name="height" step="0.01" rows="1" cols="3"/><br><br>
+    
+        <input  type="submit" name="btn_submit" value="Submit">
         
-        $BMI =  ($weight/($height*$height));
-        
-        echo "The total value is: ".$BMI;
-    }
-?>
-
-
-</body>
+    </form>
+    
+     <?php echo "The total value is: ".$BMI; ?>
+  
+  </body>
+  
 </html>
